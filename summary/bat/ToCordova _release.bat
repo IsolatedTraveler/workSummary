6777997ecaf011233fb274b2c52dd007887c2575
IsolatedTraveler <tbydmi@gmail.com>
2018-08-13 18:07:02 +0800

    20180813

---
+++ summary/bat/ToCordova _release.bat
@@ -0,0 +1,14 @@
+@echo off
+set "vueAddress=D:\Vue\ydgw"
+set "appName=ydgw"
+set "keystore=ydgw.release.keystore"
+set "alias=ydgw"
+set "storePassword=wonderscd"
+set "password=wonderscd"
+set "printAddress=C:\Users\wdgw\Desktop"
+set "VueBuild=D: && cd D:\publish "
+set "moveVue=call clearFiles.bat D:\cordova\%appName%\www && call copy.bat %vueAddress%\dist D:\cordova\%appName%\www"
+set "cordovaBuild=call cordovaBuild_release.bat %appName% %keystore% %alias% %storePassword% %password% && D: && cd D:\publish"
+set "moveApk=call copy.bat %appName% true release"
+set "exe=%VueBuild% && %moveVue% && %cordovaBuild% && %moveApk%" 
+%exe%
